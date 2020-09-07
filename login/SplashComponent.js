b992f2272156a91e396294546ed3579a9526faf7
javaPhpMyAdmin <42628668+javaPhpMyAdmin@users.noreply.github.com>
2020-09-07 04:12:45 -0300

    Update SplashComponent.js

--- login/SplashComponent.js
+++ login/SplashComponent.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
    },
    footer: {
        flex: 1,
-       backgroundColor: '#A23B1B',
+       backgroundColor: '#cccc',
        borderTopLeftRadius: 30,
        borderTopRightRadius: 30,
        paddingHorizontal: 30,
