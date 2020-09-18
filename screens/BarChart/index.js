7cf0e6210e0eb2cc54d19c981cb05fca604c6c60
javaPhpMyAdmin <42628668+javaPhpMyAdmin@users.noreply.github.com>
2020-09-18 06:48:47 -0300

    Update index.js

--- screens/BarChart/index.js
+++ screens/BarChart/index.js
@@ -6,16 +6,16 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export class BarChart extends Component {
     render() {
-        const { height, valueY, contentContainerStyle, style, labelTop, children, labelBottom, ...rest } = this.props
+        const { height, valueY, contentContainerStyle, style, labelTop, children, labelBottom, mes, ...rest } = this.props
 
         const value = height - valueY;
         return (
             <View style={[styles.barContainer, contentContainerStyle]}>
-                <TouchableOpacity onPress={() => alert('desde barChart')}>
+                <TouchableOpacity onPress={() => alert(`Reporte para el mes de: ${mes} `)}>
                     <TranslateY  startOnDidMount animateOnDidMount initialValue={height} value={value} {...rest}>
                         {labelTop}
-                        <View style={[style, { height }]}>
-                            <View style={{ height: valueY }}>
+                        <View style={[style, { height: height}]}>
+                            <View style={{ height: valueY}}>
                                 {children}
                             </View>
                         </View>
